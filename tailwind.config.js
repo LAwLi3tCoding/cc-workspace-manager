fb9495fd50422bed2285040b23864e6ba67fabba
liuyi85 <liuyi85@meituan.com>
2026-04-09 20:16:58 +0800

    feat: complete React frontend with workspace manager UI

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./client/src/**/*.{ts,tsx}', './client/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
